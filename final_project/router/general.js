5f1eb20cf4d98dddc026bcedc098a13ff16c7313
YosephBT <yosephbelay.t@gmail.com>
2025-04-05 19:55:08 +0300

    update 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,28 +10,35 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book=findBookByTitleIsbn(isbn);
+    if (book) {
+        return     res.send(book);
+    } else {
+        return res.status(404).json({message: "ISBN property doesn't exists!"});
+    }
+    res.send(book);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const book=findBookByTitleAuthor(author);
+    res.send(book);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const book=findBookByTitle(title);
+    res.send(book);
+
 });
 
 //  Get book review
@@ -39,5 +46,16 @@ public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
+
+const findBookByTitle = (title) => {
+    return Object.values(books).find(book => book.title === title);
+};
+const findBookByTitleAuthor = (author) => {
+    return Object.values(books).find(book => book.author === author);
+};
+const findBookByTitleIsbn = (isbn) => {
+    return Object.values(books).find(book => book.isbn === isbn);
+};
+
 
 module.exports.general = public_users;
